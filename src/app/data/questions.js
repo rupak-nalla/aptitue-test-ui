09083e7f0decf7887bcb0f58490199fb485e29e7
Nalla_Rupak <rupaknalla1034@gmail.com>
2024-06-10 02:33:01 +0530

    Added questions

--- src/app/data/questions.js
+++ src/app/data/questions.js
@@ -1,44 +1,104 @@
 // data/dictionary.js
 const questions= {
      1: {
-          Question: 'Question 1',
+          Question: 'A train running at the speed of 60 km/hr crosses a pole in 9 seconds. What is the length of the train?',
           Options: {
-               1:"Option1",
-               2:"Option2",
-               3:"Option3",
-               4:"Option4",
+               1:"120 metres",
+               2:"180 metres",
+               3:"324 metres",
+               4:"150 metres",
           },
-          answer:"Option1"
+          answer:"150 metres"
      },
      2: {
-          Question: 'Question 2',
+          Question: 'It was Sunday on Jan 1, 2006. What was the day of the week Jan 1, 2010?',
           Options: {
-               1:"Option1",
-               2:"Option2",
-               3:"Option3",
-               4:"Option4",
+               1:"Sunday",
+               2:"Saturday",
+               3:"Friday",
+               4:"Wednesday",
           },
-          answer:"Option2"
+          answer:"Friday"
      },
      3: {
-          Question: 'Question 3',
+          Question: 'Tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probability that the ticket drawn has a number which is a multiple of 3 or 5?',
           Options: {
-               1:"Option1",
-               2:"Option2",
-               3:"Option3",
-               4:"Option4",
+               1:"1/2",
+               2:"2/5",
+               3:"8/15",
+               4:"9/20",
           },
-          answer:"Option3"
+          answer:"9/20"
      },
      4: {
-          Question: 'Question 4',
+          Question: 'What is the value of c , If 8 is 4% of a, and 4 is 8% of b. c equals b/a.',
           Options: {
-               1:"Option1",
-               2:"Option2",
-               3:"Option3",
-               4:"Option4",
+               1:"12",
+               2:"1/4",
+               3:"0.155",
+               4:"None of these",
           },
-          answer:"Option4"
+          answer:"1/4"
+     },
+     5: {
+          Question: 'Sum of present ages of A, B and C is 92 years. If 4 years ago, the ratio of their ages were 1:2:3 respectively, find A’s present age.',
+          Options: {
+               1:"18.5 Years",
+               2:"14.8 Years",
+               3:"17.3 Years",
+               4:"20.3 Years",
+          },
+          answer:"17.3 Years"
+     },
+     6: {
+          Question: 'If each edge of a cube is increased by 50%, find the percentage increase in its surface area.',
+          Options: {
+               1:"125%",
+               2:"150%",
+               3:"175%",
+               4:"110%",
+          },
+          answer:"125%"
+     },
+     7: {
+          Question: 'The average of five numbers is 27. If one number is excluded, the average becomes 25. The excluded number is ?',
+          Options: {
+               1:"25",
+               2:"35",
+               3:"45",
+               4:"55",
+          },
+          answer:"35"
+     },
+     8: {
+          Question: 'What could be the maximum value of Y in the following equation given that neither of X, Y, Z is zero? 5X8 + 3Y4 + 2Z1 = 1103',
+          Options: {
+               1:"0",
+               2:"7",
+               3:"8",
+               4:"9",
+          },
+          answer:"7"
+     },
+     9: {
+          Question: 'SCD, TEF, UGH, ____, WKL',
+          Options: {
+               1:"CMN",
+               2:"UJI",
+               3:"VIJ",
+               4:"IJT",
+          },
+          answer:"VIJ"
+     },
+     10: {
+          Question: 'FAG, GAF, HAI, IAH, ____',
+          Options: {
+               1:"JAK",
+               2:"HAL",
+               3:"HAK",
+               4:"JAI",
+          },
+          answer:"JAK"
      }
 };
 
